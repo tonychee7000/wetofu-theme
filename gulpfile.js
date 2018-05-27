1d74ca09d51ad49a13cbc0faeee3b7fbe9999a59
TonyChyi <tonychee1989@gmail.com>
2018-05-27 21:23:16 +0800

    1

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,18 @@
 
 var gulp = require('gulp'),
     usemin = require('gulp-usemin'),
-    minifyCss = require('gulp-cssnano'),
+    minifyCss = require('gulp-clean-css'),
     minifyJs = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    minifyHTML = require('gulp-htmlmin')
+    clean = require('gulp-clean'),
+    replace = require('gulp-replace')
 
 var paths = {
     scripts: 'src/js/**/*.js',
     styles: 'src/css/**/*.css',
     partials: 'src/partials/**/*.html',
     defaults: 'src/_default/**/*.html',
-    index: 'src/index.html',
+    indexPage: 'src/index.html',
+    notFoundPage: 'src/404.html',
     header: 'src/partials/header.html',
     footer: 'src/partials/footer.html',
     fonts: 'node_modules/@fortawesome/'
@@ -25,14 +26,22 @@ var dests = {
     layouts: 'layouts/',
     partials: 'layouts/partials',
     defaults: 'layouts/_default',
+    dist: 'dist',
+    distCss: 'dist/css/**/*.css',
+    distJs: 'dist/js/**/*.js',
+    distFooter: 'dist/footer.html',
+    distHeader: 'dist/header.html'
 }
 
+/**
+ *  First Step: CSS and JS
+ */
 gulp.task('css', function() {
     return gulp.src(paths.header)
     .pipe(usemin({
-       css: [minifyCss({keepSpecialComments: 0}), 'concat']
+       css: [minifyCss(), 'concat']
     }))
-    .pipe(gulp.dest(dests.styles))
+    .pipe(gulp.dest(dests.dist))
 })
 
 gulp.task('js', function() {
@@ -40,24 +49,83 @@ gulp.task('js', function() {
     .pipe(usemin({
         js: [minifyJs(), 'concat']
     }))
-    .pipe(gulp.dest(dest.scripts))
+    .pipe(gulp.dest(dests.dist))
 })
 
 gulp.task('usemin', ['css', 'js'])
 
+/**
+ * Second Step: HTML partials
+ */
 gulp.task('html-partials', function() {
     return gulp.src(paths.partials)
-    .pipe(minifyHTML())
-    .pipe(gulp.dest(dest.partials))
+    .pipe(gulp.dest(dests.partials))
 })
 
 gulp.task('html-defaults', function() {
     return gulp.src(paths.defaults)
-    .pipe(minifyHTML())
-    .pipe(gulp.dest(dest.defaults))
+    .pipe(gulp.dest(dests.defaults))
 }) 
 
-gulp.task('html', ['html-partials', 'html-defaults'])
+gulp.task('html-index', function() {
+    return gulp.src(paths.indexPage)
+    .pipe(gulp.dest(dests.layouts))
+})
 
-gulp.task('build', ['usemin', 'html'])
+gulp.task('html-404', function() {
+    return gulp.src(paths.notFoundPage)
+    .pipe(gulp.dest(dests.layouts))
+})
+
+gulp.task('html', ['html-partials', 'html-defaults', 'html-index', 'html-404'])
+
+/**
+ * Third Step: Put file to correct path.
+ */
+gulp.task('reput-css', ['usemin'], function() {
+    return gulp.src(dests.distCss)
+    .pipe(gulp.dest(dests.styles))
+})
+
+gulp.task('reput-js', ['usemin'], function() {
+    return gulp.src(dests.distJs)
+    .pipe(gulp.dest(dests.scripts))
+})
+
+gulp.task('reput-header', ['html-partials'], function() {
+    return gulp.src(dests.distHeader)
+    .pipe(replace('css/main.min.css', '{{$baseurl}}/css/main.min.css'))
+    .pipe(gulp.dest(dests.partials))
+})
+
+gulp.task('reput-footer', ['html-partials'], function() {
+    return gulp.src(dests.distFooter)
+    .pipe(replace('js/main.min.js', '{{$baseurl}}/js/main.min.js'))
+    .pipe(gulp.dest(dests.partials))
+})
+
+gulp.task('remove-tmp', ['usemin', 'reput'], function() {
+    return gulp.src(dests.dist, {read: false})
+    .pipe(clean())
+})
+
+gulp.task('reput', ['reput-css', 'reput-js', 'reput-header', 'reput-footer'])
+
+/**
+ * Special Step: Clean all files
+ */
+gulp.task('clean-layouts', function() {
+    return gulp.src(dests.layouts, {read: false}).pipe(clean())
+})
+
+gulp.task('clean-static', function() {
+    return gulp.src(dests.static, {read: false}).pipe(clean())
+})
+
+gulp.task('clean', ['clean-layouts', 'clean-static'])
+
+/**
+ * Global Step
+ */
+gulp.task('build', ['usemin', 'html', 'reput', 'remove-tmp'])
 gulp.task('default', ['build'])
